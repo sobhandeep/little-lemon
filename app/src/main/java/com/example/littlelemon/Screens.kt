9426a025d486d5bf4d187d0b49a603aa5411fda3
Sobhandeep Gope <130676586+sobhandeep@users.noreply.github.com>
2024-10-20 23:01:12 +0530

    Setting up the onboarding screen and navigation.

--- app/src/main/java/com/example/littlelemon/Screens.kt
+++ app/src/main/java/com/example/littlelemon/Screens.kt
@@ -7,18 +7,14 @@ const val PROFILE_ROUTE = "profile"
 
 sealed class Screen(
     val route: String,
-    val title: String
 ){
     data object Home: Screen(
         route = "home_screen",
-        title = "Home Screen"
     )
     data object Onboard: Screen(
         route = "onboarding_screen",
-        title = "Onboarding Screen"
     )
     data object Profile: Screen(
         route = "profile_screen",
-        title = "Profile Screen"
     )
 }
--- app/src/main/java/com/example/littlelemon/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/littlelemon/screens/ProfileScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.littlelemon.HOME_ROUTE
